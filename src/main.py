a8921169b7a15b97b2ee6cb356afbeb5244325ae
Taiki <s.taiki0301@gmail.com>
2025-08-03 23:57:59 +0900

    initial commit

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+# from app.presentation.handler.user_handler import router
+
+
+app = FastAPI()
+# app.include_router(router)
+
+
+@app.get("/health_check")
+def ping():
+    return {'status': 'ok'}
